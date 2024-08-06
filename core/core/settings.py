9a865432614eecd80d24171239ddc69104804abc
ica1010 <ghislinametonou.com>
2024-08-06 23:57:16 +0000

    templates

--- core/core/settings.py
+++ core/core/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'whitenoise.middleware.WhiteNoiseMiddleware',
+                # 'whitenoise.middleware.WhiteNoiseMiddleware',
             ],
         },
     },
